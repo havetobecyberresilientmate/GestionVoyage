d118b38582ad1ed58408fbd0a56ce17343a86ea5
havetobecyberresilientmate <dadhibalde19@gmail.com>
2026-04-18 22:33:43 +0200

    Create Destination class with properties and constructor
    Added a Destination class with properties and a constructor.

--- ProjetVoyage/Destination.cs
+++ ProjetVoyage/Destination.cs
@@ -1,1 +1,18 @@
+class Destination
+{
+    private string _nom;
+    private string _pays;
+    private string _description;
 
+    public Destination(string nom, string pays, string description)
+    {
+        _nom = nom;
+        _pays = pays;
+        _description = description;
+    }
+
+    public void Afficher()
+    {
+        throw new NotImplementedException();
+    }
+}
