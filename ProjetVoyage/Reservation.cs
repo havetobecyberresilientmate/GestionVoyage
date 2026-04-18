02d3eaf302f1245b04591184d15f6486e0779c34
havetobecyberresilientmate <dadhibalde19@gmail.com>
2026-04-18 22:34:59 +0200

    Create Reservation class with basic functionality
    Added Reservation class with properties and methods for confirmation and cancellation.

--- ProjetVoyage/Reservation.cs
+++ ProjetVoyage/Reservation.cs
@@ -1,1 +1,25 @@
+class Reservation
+{
+    private DateTime _dateReservation;
+    private string _statut;
+    private Client _client;
+    private Voyage _voyage;
 
+    public Reservation(DateTime dateReservation, string statut, Client client, Voyage voyage)
+    {
+        _dateReservation = dateReservation;
+        _statut = statut;
+        _client = client;
+        _voyage = voyage;
+    }
+
+    public void Confirmer()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Annuler()
+    {
+        throw new NotImplementedException();
+    }
+}
