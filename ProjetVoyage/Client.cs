572b834ff9dfda29914060519e14ae274ee4e4c9
havetobecyberresilientmate <dadhibalde19@gmail.com>
2026-04-18 22:33:01 +0200

    Add Client class with constructor and methods

--- ProjetVoyage/Client.cs
+++ ProjetVoyage/Client.cs
@@ -1,1 +1,25 @@
+class Client
+{
+    private string _nom;
+    private string _prenom;
+    private string _email;
+    private string _telephone;
 
+    public Client(string nom, string prenom, string email, string telephone)
+    {
+        _nom = nom;
+        _prenom = prenom;
+        _email = email;
+        _telephone = telephone;
+    }
+
+    public void Reserver()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Annuler()
+    {
+        throw new NotImplementedException();
+    }
+}
