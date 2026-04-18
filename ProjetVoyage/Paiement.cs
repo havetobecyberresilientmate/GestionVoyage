a064fd2f7611ac9559fa97484a946a0f9ff03b81
havetobecyberresilientmate <dadhibalde19@gmail.com>
2026-04-18 22:34:22 +0200

    Add Paiement class with constructor and method

--- ProjetVoyage/Paiement.cs
+++ ProjetVoyage/Paiement.cs
@@ -1,1 +1,18 @@
+class Paiement
+{
+    private double _montant;
+    private DateTime _date;
+    private Reservation _reservation;
 
+    public Paiement(double montant, DateTime date, Reservation reservation)
+    {
+        _montant = montant;
+        _date = date;
+        _reservation = reservation;
+    }
+
+    public void EffectuerPaiement()
+    {
+        throw new NotImplementedException();
+    }
+}
