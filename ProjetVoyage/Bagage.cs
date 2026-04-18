4fee859721deff7f9ea7b542b3b5b9b6fa2464b0
havetobecyberresilientmate <dadhibalde19@gmail.com>
2026-04-18 22:32:18 +0200

    Add Bagage class with weight and type properties

--- ProjetVoyage/Bagage.cs
+++ ProjetVoyage/Bagage.cs
@@ -1,1 +1,16 @@
+class Bagage
+{
+    private double _poids;
+    private string _type;
 
+    public Bagage(double poids, string type)
+    {
+        _poids = poids;
+        _type = type;
+    }
+
+    public void VerifierPoids()
+    {
+        throw new NotImplementedException();
+    }
+}
